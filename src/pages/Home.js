64591983c2d506a07b4fc548eb548d26d34a5037
Anjali Patel <anjali.patel@berkeley.edu>
2021-06-26 14:37:35 -0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,13 +9,13 @@ function Home() {
         <img src='/img/intro-bg.jpg'/>
         <h1>Snagly</h1>
       </div>
-      <div className="about-us-container">
+      <div className="about-us">
         <h1>About Us</h1>
       </div>
-      <div className="how-it-works-container">
+      <div className="how-it-works">
         <h1>How It Works</h1>
       </div>
-      <div className="footer-container">
+      <div className="footer">
         <h1>Snagly</h1>
       </div>
     </div>
