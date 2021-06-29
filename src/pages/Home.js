601b2711bf71da4134ed32f5549f52c22726191f
Rebecca Hu <rhu01@berkeley.edu>
2021-06-29 18:32:44 -0400

    bread

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,18 @@ import './Home.css';
 
 function Home() {
   return (
+
     <div className="home">
       <div className="hero-container">
         <img src='/img/intro-bg.jpg'/>
         <h1>Snagly</h1>
         <h4>Delivered to you <br></br> in 10 minutes!</h4>
-
       </div>
       <div className="about-us">
         <h1>About Us</h1>
+        <div className="order-image">
+          <img src='/img/order.png'/>
+        </div>
       </div>
       <div className="how-it-works">
         <h1>How It Works</h1>
