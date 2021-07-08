6075790dbfacb2f386da1b2d07941d80a6fe5600
Michael Zhu <michaelwzhu@berkeley.edu>
2021-07-08 10:09:44 -0700

    Revert "lambda for createUser added"
    This reverts commit e0685cc01f1cb0c6c9df384a66225cd61c6ef828.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import { CartProvider } from './context/cart';
 import { ItemProvider } from './context/items';
 
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
-
 ReactDOM.render(
   <ItemProvider>
     <CartProvider>
